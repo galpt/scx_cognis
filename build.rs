01a8956b1e52478787fa606f693220d94cfae9ac
galpt <galpt@users.noreply.github.com>
2026-03-04 01:50:42 +0700

    init: project foundation with GPL-2.0-only license

---
+++ build.rs
@@ -0,0 +1,9 @@
+// Copyright (c) scx_cognis contributors
+// SPDX-License-Identifier: GPL-2.0-only
+
+fn main() {
+    scx_rustland_core::RustLandBuilder::new()
+        .unwrap()
+        .build()
+        .unwrap();
+}
